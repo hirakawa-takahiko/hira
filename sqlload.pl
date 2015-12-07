f43b4bd54801e3d088d0d68d9c7eaeaa129071f6
hirakawa takahiko <hirakawa@ca-reward.co.jp>
2015-12-07 13:01:20 +0900

    いろいろデバッグ

--- sqlload.pl
+++ sqlload.pl
@@ -7,12 +7,14 @@
 use DBI;
 use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
 use Encode::Guess qw(cp932 shift-jis ascii utf8);
+use Encode qw(encode decode);
 use Data::Dumper;
 
 our $SCHEMA = 'aff';
 our $USER   = 'mysql';
 our $PWD    = 'EiV3X8q1';
 our $HOST   = 'localhost';
+our $CODE   = 'sjis';
 
 GetOptions(
 	"database=s" => \$SCHEMA,
@@ -25,27 +27,29 @@ GetOptions(
 my $dbh = dbConnect();
 $dbh.do("SET NAMES sjis");
 
+
 # SQL文入りファイル読み込み
 my $sql = '';
 my $ctr = 1;	# 行カウンター
 while (my $line = <>) {
 	$ctr++;
+	$line = decode($CODE, $line);
+
 	# ファイル中のコメント行読み飛ばし
 	next if ($line =~ m|^/\*.*\*/|);
 
-	# SQL文抽出
+	# SQL文抽出(複数行SQLも考慮)
 	my $seq;
-	if ($seq = ($line =~ m/^(SELECT|INSERT|UPDATE|DELETE)/i .. $line =~ m/;\s*$/)) {
+	if ($seq = ($line =~ m/^(SELECT|INSERT|UPDATE|DELETE)/i .. $line =~ m/WHERE .*;\s*$/)) {
 		# TEXT型カラムの改行処理
 		if ($seq !~ /E0/) {		# $seqにE0が付くのは範囲マッチの最終行のみ
-			if ($line =~ /\r\n$/ or $line =~ /\n$/) {
-				$line =~ s/\r//g;
-				$line =~ s/\n/\\n/g;
-			}
+			$line =~ s/\r\n/\n/g if ($line=~ /\r\n/);	#^M(CRLF)をLFに変換
+			$line =~ s/\n/\\n/g if ($line =~ /\n$/);	# 改行を \nに変換
 		}
+		$line = encode('sjis', $line);
 		$sql = join(' ', ($sql, $line));
 	} 
-	if ($seq =~ /E0/) {		# 範囲マッチング終了
+	if ($seq =~ /E0/) {		# 範囲マッチング終了時に..演算子はシーケンス番号にE0をつけて返す
 		# SQL実行
 		my $status = $dbh->do($sql);
 		unless (defined($status)) {
